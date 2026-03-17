4e9b13c37315aa5eb13adb710f92514f1b81de7b
mishalturkane <mishalturkane@gmail.com>
2026-03-18 01:11:07 +0530

    fix: fixed main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 mod _00_intro;
 mod _01_variables;
-mod _02_loops;
-mod _03_functions;
+mod _03_loops;
+mod _04_functions;
 
 fn main() {
 
-   let sum = _03_functions::take_something_return_something(10,20);
+   let sum = _04_functions::take_something_return_something(10,20);
    println!("tsrs fn - the sum of a&b is: {}", sum);
    
-   _03_functions::take_nothing_return_nothing();
+   _04_functions::take_nothing_return_nothing();
    
-   _03_functions::take_something_return_nothing(10, 20);
+   _04_functions::take_something_return_nothing(10, 20);
    
-   let sum = _03_functions::take_nothing_return_something();
+   let sum = _04_functions::take_nothing_return_something();
    println!("tnrs fn - the sum of a&b is: {}", sum);
 }
