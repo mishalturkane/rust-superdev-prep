58784e69b5a48a6a8686ae96ac2d8cb43ec87527
mishalturkane <mishalturkane@gmail.com>
2026-03-18 00:45:15 +0530

    feat: take nothing return nothing fn added

--- src/_03_functions/functions.rs
+++ src/_03_functions/functions.rs
@@ -1,3 +1,12 @@
+#[allow(dead_code)]
 pub fn take_something_return_something(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[allow(dead_code)]
+pub fn take_nothing_return_nothing(){
+    let a = 10; 
+    let b = 20;
+    let sum = a + b;
+    println!("tnrn fn - the sum of a&b is: {}", sum);
 }
