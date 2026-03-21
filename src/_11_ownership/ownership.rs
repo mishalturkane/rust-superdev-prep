e723a97791d94cc670b4679fc2e7809034a6e42e
mishalturkane <mishalturkane@gmail.com>
2026-03-22 02:20:01 +0530

    updt: ownership fn updated

--- src/_11_ownership/ownership.rs
+++ src/_11_ownership/ownership.rs
@@ -1,5 +1,7 @@
 pub fn ownership() {
     let s1 = String::from("hello");
+    s1_in_stack(&s1);
+    s1_data_in_heap(&s1);
     let address_of_s1 = s1.as_ptr();
     let s2 = s1;
     let address_of_s2 = s2.as_ptr();
@@ -15,4 +17,18 @@ pub fn ownership() {
     
     println!("now s2 data is: {}",s2);
     
+}
+
+fn s1_in_stack(s: &String) {
+    let address = s.as_ptr();
+    let len = s.len();
+    let cap = s.capacity();
+    println!("Stack Metadata -> Address: {:p}, Len: {}, Cap: {}", address, len, cap);
+}
+
+fn s1_data_in_heap(s: &String) {
+    println!("address in heap: {:p}",s.as_ptr());
+    for i in 0..s.len() {
+        println!("Heap Data -> Index: {}, Value: {}", i, s.chars().nth(i).unwrap());
+    }
 }
